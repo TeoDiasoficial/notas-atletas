619ad462376709165acd96d498d7ac03027723d2
TeoDiasoficial <122123373+TeoDiasoficial@users.noreply.github.com>
2023-01-12 21:26:20 -0300

    Update notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,57 +1,50 @@
-// Funcao responsavel pelo calculo das notas
-function calculaNotas(objGinastas) {
- // Percorre a lista de ginastas
- for (let i = 0; i < objGinastas.length; i++) {
+function calculaNotas(objAtletas) {
+  for (let i = 0; i < objAtletas.length; i++) {
 
-   let ginasta = objGinastas[i]['nome']
-   let notas = objGinastas[i]['notas']
+    let atleta = objAtletas[i]['nome']
+    let notas = objAtletas[i]['notas']
+   
+   var nome = ['nome']
+  nome.sort(function(a, b)
+              {return a-b});
+    var Atletas = ['notas']
+  notas.sort(function(a,b)
+              {return a-b});
 
-   // Ordenando as notas
-   notas = notas.sort()
+   
+    let notasComputadas = notas.slice(1, 4)
+    var acumulador = 0
+    notasComputadas.forEach(notas => {
+      acumulador = notas + acumulador
+    })
 
-   // Eliminando a maior e menor nota
-   let notasComputadas = notas.slice(1,4)
-  
-   // Calculando a media
-   // Acumulador para salvar a soma das notas
-   var acumulador = 0
-   // Metodo forEach para somar as notas
-   notasComputadas.forEach(nota => {
-     acumulador = nota + acumulador
-   })
+    let mediaValida = acumulador / notasComputadas.length
 
-   // Calculo da media utilizando o acumulador e a quantidade de notas
-   let mediaValida = acumulador / notasComputadas.length
-
-
-   // Imprime o Resultado
-   console.log(`
-     Ginasta: ${ginasta}
-     Notas Obtidas: ${notas}
-     Média Válida: ${mediaValida}
-   `)
+    console.log(`
+        Atleta: ${atleta}
+        Notas Obtidas: ${notas}
+        Media Válida: ${mediaValida}
+      `)
   }
 }
 
-// Insercao de Ginastas e Notas
-let ginastas = [
- {
-   nome: "Cesar Abascal",
-   notas: [10, 9.34, 8.42, 10, 7.88]
- },
- {
-   nome: "Fernando Puntel",
-   notas:  [8, 10, 10, 7, 9.33]
- },
- {
-   nome: "Daiane Jelinky",
-   notas: [7, 10, 9.5, 9.5, 8]
- },
- {
-   nome: "Bruno Castro",
-   notas: [10, 10, 10, 9, 9.5]
- }
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
 ];
 
-// Chamada da Funcao
-calculaNotas(ginastas);
+calculaNotas(atletas);
